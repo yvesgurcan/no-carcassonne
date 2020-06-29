61dec8ad92c820f3b4909fc95e3bca460f135e9f
yvesgurcan <gurcan.yves+github@gmail.com>
2020-06-28 18:25:55 -0700

    Avoid repetition of Map

--- src/tileTypes.js
+++ src/tileTypes.js
@@ -1,68 +1,87 @@
-const { EMPTY_NODE, ROAD, MONASTERY } = require('./nodeTypes');
+const { EMPTY_NODE, CITY, ROAD, MONASTERY } = require('./nodeTypes');
 
 /**
- * @returns {Map} A tile with the default feature.
+ * @returns {Map} A tile where all nodes are populated with the default feature.
  */
-function generateEmptyTile() {
+function generateEmptyTileTemplate() {
     let tile = new Map();
     const connections = new Array(13).fill({ ...EMPTY_NODE });
+    tile.set('name', 'Empty tile');
     connections.forEach((connection, index) => {
         tile.set(index, connection);
     });
     return tile;
 }
 
-const EMPTY_TILE = generateEmptyTile();
+const EMPTY_TILE = generateEmptyTileTemplate();
 
 /**
  *
- * @param {Map} map A list of nodes with specific features to generate the tile.
+ * @param {Array} tileData A list of nodes with specific features to generate the tile.
  * @returns {Map} A list of all the nodes that make the tile, including nodes using the default feature.
  */
-function generateTileFromMap(map) {
+function generateTileTemplate(tileData) {
     const connections = new Map(EMPTY_TILE);
-    map.forEach((feature, index) => {
-        const connection = connections.get(index);
-        connections.set(index, { ...connection, feature });
+    tileData.map(([key, value]) => {
+        const connection = connections.get(key);
+
+        // Only numbered properties are nodes
+        if (String(key).match(/^\d+$/)) {
+            // Keep other properties on the node
+            connections.set(key, { ...connection, feature: value });
+        } else {
+            connections.set(key, value);
+        }
     });
 
     return connections;
 }
 
-const THREE_WAY_CROSSROAD_MAP = new Map([
+const START_TILE_MAP = [
+    ['name', 'Start tile'],
+    [1, ROAD],
+    [5, ROAD],
+    [6, ROAD],
+    [7, CITY]
+];
+
+const THREE_WAY_CROSSROAD_MAP = [
     ['name', 'Three-way crossroad'],
     [1, ROAD],
     [5, ROAD],
     [6, ROAD],
     [7, ROAD]
-]);
+];
 
-const FOUR_WAY_CROSSROAD_MAP = new Map([
+const FOUR_WAY_CROSSROAD_MAP = [
     ['name', 'Four-way crossroad'],
     [1, ROAD],
     [5, ROAD],
     [6, ROAD],
     [7, ROAD],
     [11, ROAD]
-]);
+];
 
-const MONASTERY_WITHOUT_ROAD_MAP = new Map([
-    ['name', 'Monsatery'],
+const MONASTERY_WITHOUT_ROAD_MAP = [
+    ['name', 'Monastery'],
     [6, MONASTERY]
-]);
+];
 
-const MONASTERY_WITH_ROAD_MAP = new Map([
-    ['name', 'Monsatery'],
+const MONASTERY_WITH_ROAD_MAP = [
+    ['name', 'Monastery with road'],
     [1, ROAD],
     [6, MONASTERY]
-]);
+];
 
-const THREE_WAY_CROSSROAD = generateTileFromMap(THREE_WAY_CROSSROAD_MAP);
-const FOUR_WAY_CROSSROAD = generateTileFromMap(FOUR_WAY_CROSSROAD_MAP);
-const MONASTERY_WITHOUT_ROAD = generateTileFromMap(MONASTERY_WITHOUT_ROAD_MAP);
-const MONASTERY_WITH_ROAD = generateTileFromMap(MONASTERY_WITH_ROAD_MAP);
+const START_TILE = generateTileTemplate(START_TILE_MAP);
+const THREE_WAY_CROSSROAD = generateTileTemplate(THREE_WAY_CROSSROAD_MAP);
+const FOUR_WAY_CROSSROAD = generateTileTemplate(FOUR_WAY_CROSSROAD_MAP);
+const MONASTERY_WITHOUT_ROAD = generateTileTemplate(MONASTERY_WITHOUT_ROAD_MAP);
+const MONASTERY_WITH_ROAD = generateTileTemplate(MONASTERY_WITH_ROAD_MAP);
 
 const TILE_TYPES = [
+    EMPTY_TILE,
+    START_TILE,
     FOUR_WAY_CROSSROAD,
     THREE_WAY_CROSSROAD,
     MONASTERY_WITHOUT_ROAD,
