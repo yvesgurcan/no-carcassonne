92664a2bbb188cb336411c63927671a83b54c889
yvesgurcan <gurcan.yves+github@gmail.com>
2020-06-29 23:01:52 -0700

    Create renderer functions

---
+++ src/renderer/index.js
@@ -0,0 +1,205 @@
+import { CITY, ROAD, MONASTERY, RIVER } from '../engine/nodeTypes';
+import { isNumber } from '../util';
+
+const TILE_SIZE = 60;
+
+function getElement(id) {
+    const element = document.getElementById(id);
+
+    if (!element) {
+        throw new Error(`Element '#${id}' not found.`);
+    }
+
+    return element;
+}
+
+function createElement(type, id) {
+    const element = document.createElement(type);
+    element.id = id;
+
+    return element;
+}
+
+function getOffset(offsetMultiplier) {
+    const offsetBase = TILE_SIZE / 3;
+    return offsetBase * offsetMultiplier;
+}
+function drawNode(index, node, canvas) {
+    switch (node.feature) {
+        default: {
+            return;
+        }
+        case ROAD: {
+            canvas.fillStyle = 'rgb(200, 180, 90)';
+            break;
+        }
+        case CITY: {
+            canvas.fillStyle = 'rgb(120, 80, 40)';
+            break;
+        }
+        case MONASTERY: {
+            canvas.fillStyle = 'rgb(140, 160, 170)';
+            break;
+        }
+        case RIVER: {
+            canvas.fillStyle = 'rgb(40, 60, 230)';
+            break;
+        }
+    }
+
+    switch (index) {
+        default: {
+            // Don't draw
+            return;
+        }
+        case 0: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(0), getOffset(0));
+            canvas.lineTo(getOffset(1), getOffset(1));
+            canvas.lineTo(getOffset(1), getOffset(0));
+            canvas.fill();
+            return;
+        }
+        case 1: {
+            canvas.fillRect(
+                getOffset(1),
+                getOffset(0),
+                getOffset(1),
+                getOffset(1)
+            );
+            return;
+        }
+        case 2: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(2), getOffset(0));
+            canvas.lineTo(getOffset(2), getOffset(1));
+            canvas.lineTo(getOffset(3), getOffset(0));
+            canvas.fill();
+            return;
+        }
+        case 3: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(0), getOffset(0));
+            canvas.lineTo(getOffset(0), getOffset(1));
+            canvas.lineTo(getOffset(1), getOffset(1));
+            canvas.fill();
+            return;
+        }
+        case 4: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(3), getOffset(0));
+            canvas.lineTo(getOffset(2), getOffset(1));
+            canvas.lineTo(getOffset(3), getOffset(1));
+            canvas.fill();
+            return;
+        }
+        case 5: {
+            canvas.fillRect(
+                getOffset(0),
+                getOffset(1),
+                getOffset(1),
+                getOffset(1)
+            );
+            return;
+        }
+        case 6: {
+            canvas.fillRect(
+                getOffset(1),
+                getOffset(1),
+                getOffset(1),
+                getOffset(1)
+            );
+            return;
+        }
+        case 7: {
+            canvas.fillRect(
+                getOffset(2),
+                getOffset(1),
+                getOffset(1),
+                getOffset(1)
+            );
+            return;
+        }
+        case 8: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(0), getOffset(2));
+            canvas.lineTo(getOffset(1), getOffset(2));
+            canvas.lineTo(getOffset(0), getOffset(3));
+            canvas.fill();
+            return;
+        }
+        case 9: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(2), getOffset(2));
+            canvas.lineTo(getOffset(3), getOffset(2));
+            canvas.lineTo(getOffset(3), getOffset(3));
+            canvas.fill();
+            return;
+        }
+        case 10: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(0), getOffset(3));
+            canvas.lineTo(getOffset(1), getOffset(3));
+            canvas.lineTo(getOffset(1), getOffset(2));
+            canvas.fill();
+            return;
+        }
+        case 11: {
+            canvas.fillRect(
+                getOffset(1),
+                getOffset(2),
+                getOffset(1),
+                getOffset(1)
+            );
+            return;
+        }
+        case 12: {
+            canvas.beginPath();
+            canvas.moveTo(getOffset(2), getOffset(2));
+            canvas.lineTo(getOffset(2), getOffset(3));
+            canvas.lineTo(getOffset(3), getOffset(3));
+            canvas.fill();
+            return;
+        }
+    }
+}
+
+function drawTile(tile, id) {
+    const element = createElement('canvas', id);
+    element.style.backgroundColor = 'green';
+    element.style.width = `${TILE_SIZE}px`;
+    element.style.height = `${TILE_SIZE}px`;
+    element.width = TILE_SIZE;
+    element.height = TILE_SIZE;
+    const canvas = element.getContext('2d');
+
+    tile.forEach((value, key) => {
+        if (isNumber(key)) {
+            drawNode(key, value, canvas);
+        }
+    });
+    return element;
+}
+
+function drawWorldTiles(worldState, container) {
+    worldState.forEach((value, key) => {
+        if (isNumber(key)) {
+            const offset = key * 20;
+            const tile = drawTile(value, `world${key}`);
+            tile.style.marginLeft = offset;
+            tile.style.marginTop = offset;
+            container.append(tile);
+        }
+    });
+}
+
+export function render(gameState) {
+    const tilesInStack = getElement('tiles-in-stack');
+    tilesInStack.innerHTML = gameState.stack.size;
+
+    const seed = getElement('seed');
+    seed.innerHTML = gameState.seed;
+
+    const world = getElement('world');
+    drawWorldTiles(gameState.world, world);
+}
