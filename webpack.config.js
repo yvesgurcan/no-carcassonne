4c96c0faaa65f9ea1a363c9da9a32c3bcf829d3d
yvesgurcan <gurcan.yves+github@gmail.com>
2020-06-29 23:00:31 -0700

    Add Webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, './')
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: [
+                'main.*.js',
+                'vendors~main.*.js',
+                'runtime.*.js'
+            ]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    optimization: {
+        // Separate runtime code into a runtime chunk
+        runtimeChunk: 'single',
+        // Separate dependencies into a vendors chunk
+        splitChunks: {
+            chunks: 'all'
+        }
+    },
+    devServer: {
+        overlay: true,
+        stats: 'minimal',
+        host: '0.0.0.0',
+        disableHostCheck: true
+    }
+};
