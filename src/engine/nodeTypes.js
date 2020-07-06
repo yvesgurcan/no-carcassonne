1d56b9aa5ed41b6db1bf1e588e877256028193e0
yvesgurcan <gurcan.yves+github@gmail.com>
2020-07-05 20:13:37 -0700

    Update node types: add crossroad and owners is an array

--- src/engine/nodeTypes.js
+++ src/engine/nodeTypes.js
@@ -2,8 +2,9 @@ export const GRASS = 'G';
 export const BLANK = 'B';
 export const CITY = 'C';
 export const ROAD = 'R';
+export const CROSSROAD = 'X';
 export const MONASTERY = 'M';
 export const RIVER = 'S'; // 'S' for 'Stream'
 export const GARDEN = 'G';
 
-export const EMPTY_NODE = { feature: GRASS, owner: undefined };
+export const EMPTY_NODE = { feature: GRASS, owners: [] };
